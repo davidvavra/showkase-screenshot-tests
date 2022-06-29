acd7c717bcd20d4bcbb6e423706d466818b05e8b
David Vavra <david@vavra.me>
2022-06-29 19:29:07 +0200

    CI integration fixes (#5)
    * Update Feature.kt
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Update process_failed_screenshots.sh
    
    * WIP
    
    * Update Feature.kt
    
    * Update screenshots (#6)

--- .github/workflows/scripts/process_failed_screenshots.sh
+++ .github/workflows/scripts/process_failed_screenshots.sh
@@ -5,11 +5,6 @@ if [[ -z ${GITHUB_TOKEN} ]]; then
   exit 1
 fi
 
-if [[ -z ${GITHUB_ACTOR} ]]; then
-  echo "Missing GITHUB_ACTOR variable"
-  exit 1
-fi
-
 if [[ -z ${GITHUB_REPOSITORY} ]]; then
   echo "Missing GITHUB_REPOSITORY variable"
   exit 1
@@ -20,14 +15,19 @@ if [[ -z ${PR_BRANCH} ]]; then
   exit 1
 fi
 
+./gradlew recordPaparazziDebug
+
 PR_NUMBER=${GITHUB_REF#refs/pull/}
 PR_NUMBER=${PR_NUMBER/\/merge/}
 NEW_BRANCH_NAME="snapshots/pr-$PR_NUMBER"
+echo "::set-output name=PR_NUMBER::$PR_NUMBER"
 
+git config user.name "CI/CD"
+git config user.email "david@vavra.me" # TODO: replace with some CI/CD GitHub user
 git fetch --all
 git checkout --track "origin/$PR_BRANCH"
 git checkout -b "$NEW_BRANCH_NAME"
 git add -A
 git commit -m "Update screenshots"
-git push --force "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git push --force "https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 echo "::set-output name=PR_COMMENT::\"Screenshot tests failed.\\n\\n[See differences](https://github.com/$GITHUB_REPOSITORY/compare/$PR_BRANCH...$NEW_BRANCH_NAME)\\n\\nMerge the branch if it's an intentional change.\""
