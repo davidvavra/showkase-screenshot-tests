39912c32b61626ea47b296156eabac9ea36b28ef
David Vavra <david@vavra.me>
2022-06-29 18:38:55 +0200

    WIP

--- .github/workflows/scripts/process_failed_screenshots.sh
+++ .github/workflows/scripts/process_failed_screenshots.sh
@@ -19,10 +19,12 @@ PR_NUMBER=${GITHUB_REF#refs/pull/}
 PR_NUMBER=${PR_NUMBER/\/merge/}
 NEW_BRANCH_NAME="snapshots/pr-$PR_NUMBER"
 
+git config user.email "ci-cd@vavra.me"
 git fetch --all
 git checkout --track "origin/$PR_BRANCH"
 git checkout -b "$NEW_BRANCH_NAME"
 git add -A
 git commit -m "Update screenshots"
 git push --force "https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+echo "::set-output name=PR_NUMBER::$PR_NUMBER"
 echo "::set-output name=PR_COMMENT::\"Screenshot tests failed.\\n\\n[See differences](https://github.com/$GITHUB_REPOSITORY/compare/$PR_BRANCH...$NEW_BRANCH_NAME)\\n\\nMerge the branch if it's an intentional change.\""
